8d298e6da8e7343eb1e381de5f7b3092503845f2
yoonjeewoo <yoonjeewoo@gmail.com>
2017-03-09 17:13:04 +0900

    second commit

--- 1.c
+++ 1.c
@@ -3,7 +3,9 @@ int main() {
    int a;
    scanf(“%d”,&a);
    if(a == 1) {
-      printf(“Jeewoo Yoon”);
+      printf("Jeewoo Yoon");
+   }else (a == 2) {
+       printf("Hello my name is Jeewoo Yoon");
    }
    return 0;
 }
